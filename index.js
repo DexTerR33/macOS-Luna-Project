247d143f10b1064bd8b93de5bc35c1e1ff2081f5
Iordache Marian <77465757+DexTerR33@users.noreply.github.com>
2022-01-25 23:29:50 +0200

    first commit for macOS Luna project

---
+++ index.js
@@ -0,0 +1,20 @@
+function myFunction() {
+    document.getElementById("myDropsdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropsbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-constent");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+
+
+
