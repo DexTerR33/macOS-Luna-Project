23613599994d6a8b526d8d6449bc18f41e6215de
Iordache Marian <77465757+DexTerR33@users.noreply.github.com>
2022-02-06 18:22:51 +0200

    some updates + top nav dragdown

--- index.js
+++ index.js
@@ -1,20 +1,41 @@
-function myFunction() {
-    document.getElementById("myDropsdown").classList.toggle("show");
+(function() {
+
+  var dropBtns = document.querySelectorAll('.dropdown-button');
+
+  function closeOpenItems() {
+      openMenus = document.querySelectorAll('.dropMenu-list');
+      openMenus.forEach(function(menus) {
+        menus.classList.remove('show');
+      });  
   }
-  
-  // Close the dropdown if the user clicks outside of it
-  window.onclick = function(event) {
-    if (!event.target.matches('.dropsbtn')) {
-      var dropdowns = document.getElementsByClassName("dropdown-constent");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
+
+  dropBtns.forEach(function(btn) {
+
+    btn.addEventListener('click', function(e) {
+      var 
+        dropContent = btn.querySelector('.dropMenu-list'),
+        shouldOpen = !dropContent.classList.contains('show');
+      e.preventDefault();
+
+      // First close all open items.
+      closeOpenItems();
+      // Check if the clicked item should be opened. It is already closed at this point so no further action is required if it should be closed.
+      if (shouldOpen) {
+        // Open the clicked item.
+        dropContent.classList.add('show');      
       }
+      e.stopPropagation();
+    });
+
+
+  });
+
+  //   close menus when clicking outside of them
+  window.addEventListener('click', function(event) {
+    if (event.target != dropBtns) {
+      // Moved the code here to its own function.
+      closeOpenItems();
     }
-  }
+  });
 
-
-
+})();
