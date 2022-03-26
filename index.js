d74d46de93cc4724791c9982b104ea7cadc4cdcf
DexTerR33 <pusaw202@gmail.com>
2022-03-26 03:11:33 +0200

    some upgrade

--- index.js
+++ index.js
@@ -1,109 +1,98 @@
 
 
-
-
-
-/*
-function moveWindows() {
-  var dropBtns = document.querySelectorAll('.dropdown-button');
-  function closeOpenItems() {
-    openMenus = document.querySelectorAll('.dropMenu-list');
-    openMenus.forEach(function (menus) {
-      menus.classList.remove('show');
+  function moveWindows() {
+    var dropBtns = document.querySelectorAll('.dropdown-button');
+console.log(dropBtns , "btnSho")
+    function closeOpenItems() {
+      openMenus = document.querySelectorAll('.dropMenu-list');
+      openMenus.forEach(function (menus) {
+        menus.classList.remove('show');
+      });
+    }
+    dropBtns.forEach(function (btn) {
+      btn.addEventListener('click', function (e) {
+        var
+          dropContent = btn.querySelector('.dropMenu-list'),
+          shouldOpen = !dropContent.classList.contains('show');
+        e.preventDefault();
+        // First close all open items.
+        closeOpenItems();
+        // Check if the clicked item should be opened. It is already closed at this point so no further action is required if it should be closed.
+        if (shouldOpen) {
+          // Open the clicked item.
+          dropContent.classList.add('show');
+        }
+        e.stopPropagation();
+      });
     });
-  }
-  dropBtns.forEach(function (btn) {
-    btn.addEventListener('click', function (e) {
-      var
-        dropContent = btn.querySelector('.dropMenu-list'),
-        shouldOpen = !dropContent.classList.contains('show');
-      e.preventDefault();
-      // First close all open items.
-      closeOpenItems();
-      // Check if the clicked item should be opened. It is already closed at this point so no further action is required if it should be closed.
-      if (shouldOpen) {
-        // Open the clicked item.
-        dropContent.classList.add('show');
+    window.addEventListener('click', function (event) {
+      if (event.target != dropBtns) { 
+        closeOpenItems();
       }
-      e.stopPropagation();
     });
-  });
-  // close menus when clicking outside of them
-  window.addEventListener('click', function (event) {
-    if (event.target != dropBtns) {
-  // Moved the code here to its own function.
-      closeOpenItems();
-    }
-  });
-dragElement(document.getElementById("finderclone"));
-dragElement(document.getElementById("finderclonetest"));
-  function dragElement(elmnt) {
-    var pos1 = 0,
-      pos2 = 0,
-      pos3 = 0,
-      pos4 = 0;
-    if (document.getElementById(elmnt.id + "header")) {
-      // if present, the header is where you move the DIV from:
+    dragElement(document.getElementById("finderclone"));
+    dragElement(document.getElementById("finderclonetest"));
+    function dragElement(elmnt) {
+      var pos1 = 0,
+        pos2 = 0,
+        pos3 = 0,
+        pos4 = 0;
+      if (document.getElementById(elmnt.id + "header")) {
       document.getElementById(elmnt.id + "header").onmousedown =
-        dragMouseDown;
-    } else {
-      // otherwise, move the DIV from anywhere inside the DIV:
-      elmnt.onmousedown = dragMouseDown;
-    }
-    function dragMouseDown(e) {
-      e = e || window.event;
-      e.preventDefault();
-      // get the mouse cursor position at startup:
-      pos3 = e.clientX;
-      pos4 = e.clientY;
-      document.onmouseup = closeDragElement;
-      // call a function whenever the cursor moves:
-      document.onmousemove = elementDrag;
-    }
-    function elementDrag(e) {
-      e = e || window.event;
-      e.preventDefault();
-      // calculate the new cursor position:
-      pos1 = pos3 - e.clientX;
-      pos2 = pos4 - e.clientY;
-      pos3 = e.clientX;
-      pos4 = e.clientY;
-      // set the element's new position:
-      elmnt.style.top = elmnt.offsetTop - pos2 + "px";
-      elmnt.style.left = elmnt.offsetLeft - pos1 + "px";
-    }
-    function closeDragElement() {
-    // stop moving when mouse button is released:
-      document.onmouseup = null;
-      document.onmousemove = null;
-    }
-  }
+          dragMouseDown;
+      } else {
   
-  //Make the DIV element draggagle:
-$(".press-me-button").click(function() {
-  $("#newcontact").toggle();
-});
-  // Close the dropdown if the user clicks outside of it
-  $(".container-aboutMe1").click(function () {
-    $(".overlay1").toggleClass("active2");
-    $(".overlay1").addClass("active");
-    console.log("overlay", "pass1");
-  });
+        elmnt.onmousedown = dragMouseDown;
+      }
+      function dragMouseDown(e) {
+        e = e || window.event;
+        e.preventDefault();
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        document.onmouseup = closeDragElement;
+        document.onmousemove = elementDrag;
+      }
 
-  $(".close").click(function () {
-    $(".overlay1").addClass("active2");
-    console.log("overlay", "pass2");
-  });
-  $("#dropDown1").click(function () {
-    $(".drop-down").toggleClass("drop-down--active");
-  });
-  $("#dropDown2").click(function () {
-    $(".drop-down2").toggleClass("drop-down--active");
-  });
-}
-moveWindows();
-*/
+      function elementDrag(e) {
+        e = e || window.event;
+        e.preventDefault();
+        pos1 = pos3 - e.clientX;
+        pos2 = pos4 - e.clientY;
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        elmnt.style.top = elmnt.offsetTop - pos2 + "px";
+        elmnt.style.left = elmnt.offsetLeft - pos1 + "px";
+      }
 
+      function closeDragElement() {
+        document.onmouseup = null;
+        document.onmousemove = null;
+      }
+    }
+
+    $(".press-me-button").click(function () {
+      $("#newcontact").toggle();
+    });
+
+    $(".container-aboutMe").click(function () {
+      $(".overlay").toggleClass("active2");
+      $(".overlay").addClass("active");
+      console.log("overlay", "pass1");
+    });
+
+    $(".close").click(function () {
+      $(".overlay").addClass("active2");
+      console.log("overlay", "pass2");
+    });
+    $("#dropDown").click(function () {
+      $(".drop-down").toggleClass("drop-down--active");
+    });
+    $("#dropDown2").click(function () {
+      $(".drop-down2").toggleClass("drop-down--active");
+    });
+  }
+
+  moveWindows() ;
 
 
 const openPopupButtons = document.querySelectorAll('[data-popup-target]');
@@ -138,16 +127,20 @@ closePopupButtons.forEach(button => {
 });
 
 function openPopup(popupOpen) {
-  if(popupOpen == null) return;
+  if (popupOpen == null) return;
   popupClass.classList.add('active');
   overlay.classList.add('active');
   console.log(popupOpen, "2");
 }
+
 function closePopup(popupClose) {
-  if(popupClose == null) return;
+  if (popupClose == null) return;
   popupClose.classList.remove('active');
   overlay.classList.remove('active');
   console.log(popupClose, "test");
 }
+
+
+
 
 
